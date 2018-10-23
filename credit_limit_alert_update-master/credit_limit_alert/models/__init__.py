ae629186905dd7ef8f034e4e4ff8c3da11fe3953
viernes <luis.muro@xmarts.com>
2018-10-23 12:07:19 -0500

    Credit limit

---
+++ credit_limit_alert_update-master/credit_limit_alert/models/__init__.py
@@ -0,0 +1,4 @@
+from . import res_partner
+from . import sale_order
+from . import statement_partner_report
+
---
+++ credit_limit_alert_update-master/credit_limit_alert/models/res_partner.py
@@ -0,0 +1,37 @@
+from odoo import models, fields, api, _
+
+class CreditLimitAlertResPartner(models.Model):
+    _name = 'res.partner'
+    _inherit = 'res.partner'
+
+    credit_limit = fields.Monetary('Limite de credito')
+    credit_available = fields.Monetary('Cedito disponible', compute='_compute_amount_credit_available')
+
+    @api.one
+    @api.depends('credit_limit','credit_available','credit')
+    def _compute_amount_credit_available(self):
+
+        self.credit_available = self.credit_limit - self.credit
+
+        pass
+    @api.multi
+    def call_wizard(self):
+        wizard_form = self.env.ref('credit_limit_alert.credit_limit_alert_partner_statement_wizard_view', False)
+        view_id = self.env['credit_limit_alert.partner_statement_wizard']
+        vals = {
+            'name': 'this is for set name',
+            'str_partner_id': self.id,
+        }
+        new = view_id.create(vals)
+        return {
+            'name': _('Reporte de estado de deudas de ' + self.name),
+            'type': 'ir.actions.act_window',
+            'res_model': 'credit_limit_alert.partner_statement_wizard',
+            'res_id': new.id,
+            'view_id': wizard_form.id,
+            'view_type': 'form',
+            'view_mode': 'form',
+            'target': 'new'
+        }
+
+
