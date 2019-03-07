3690fc58c475a77a7d5994bafdf7f0bc3ec2a0d5
viernes <shelonga@live.com>
2019-03-07 14:27:32 -0600

    Reporte servicio debug

--- Reporte Servicio/__manifest__.py
+++ Reporte Servicio/__manifest__.py
@@ -1,46 +1,21 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "your_module_name",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
+    'name': 'Xmarts Reporte Servicio ',
+    'version': '11.0.3',
+    'category': "",
+    'description': """ Sales quotation Report 
     """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base','purchase','sale'],
-
-    # always loaded
+    'author':'Axel indian God',
+    'depends': ['base'],
     'data': [
-        # 'security/ir.model.access.csv',
-      
 	
-	
-
-
-	'reports/report_serv.xml',
-	'reports/report_serv2.xml',
-	
-
-	
-	
+	"reports/report_serv.xml",
+	  
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
-	"installable": True,
-    	"auto_install": True,
+    'qweb': [
+        ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
 }
