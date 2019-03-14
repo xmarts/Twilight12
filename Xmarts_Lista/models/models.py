fbc42904c02f03b3229b2ebf5cadf556fac68ddd
viernes <shelonga@live.com>
2019-03-14 10:57:36 -0600

    Reporte Followup2

--- Xmarts_Lista/models/models.py
+++ Xmarts_Lista/models/models.py
@@ -2,11 +2,10 @@
 
 from odoo import models, fields, api
 
- class XmartsLista(models.Model):
-     _name = 'xmarts.Lista'
-
-     name = fields.Char()
-     value = fields.Integer()
-     description = fields.Text()
+class XmartsLista(models.Model):
+	_name = 'xmarts.Lista'
+	name = fields.Char()
+	value = fields.Integer()
+	description = fields.Text()
 
 
