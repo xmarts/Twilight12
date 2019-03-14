0b09a4949732c7602351d0bd6834463675a4ed0e
viernes <shelonga@live.com>
2019-03-14 12:18:32 -0600

    Reporte Followup2

---
+++ myfirst_model/controllers/controllers.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+
+class MyfirstModel(http.Controller):
+    @http.route('/inventory/categorias', type='http', auth='public', website=True)
+    def render_example_page(self):
+        categorias = http.request.env['Categorias'].sudo().search([])
+        return http.request.render('myfirst_model.vistaCategoria',{'Categorias':categorias})
+
+
