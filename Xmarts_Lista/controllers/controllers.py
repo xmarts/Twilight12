17a8f46587e1346695ba690101c08699dc6bcb82
viernes <shelonga@live.com>
2019-03-14 10:42:41 -0600

    Reporte Followup2

--- Xmarts_Lista/controllers/controllers.py
+++ Xmarts_Lista/controllers/controllers.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 
- class YourModuleName(http.Controller):
+class YourModuleName(http.Controller):
 #   @http.route('/your_module_name/your_module_name/', auth='public')
 #     def index(self, **kw):
 #         return "Hello, world"
@@ -18,8 +18,7 @@ from odoo import http
 #         return http.request.render('your_module_name.object', {
 #             'object': obj
 #         })
-
-    @http.route('/example/detail', type='http', auth='users', website=True)
-    def navigate_to_detail_page(self):
-	modelo = http.request.env['xmarts.Lista'].sudo().search([])
-	return http.request.render('create_webpage_demo.detail_page', {'modelo':modelo})
+@http.route('/example/detail', type='http', auth='users', website=True)
+	def navigate_to_detail_page(self):
+		modelo = http.request.env['xmarts.Lista'].sudo().search([])
+		return http.request.render('create_webpage_demo.detail_page', {'modelo':modelo})
