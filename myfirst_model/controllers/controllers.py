689e2a17c30f5b7d7b5b70364dff66a920d87a53
viernes <shelonga@live.com>
2019-03-14 12:19:21 -0600

    Reporte Followup2

--- myfirst_model/controllers/controllers.py
+++ myfirst_model/controllers/controllers.py
@@ -2,7 +2,7 @@
 from odoo import http
 
 class MyfirstModel(http.Controller):
-    @http.route('/inventory/categorias', type='http', auth='public', website=True)
+    @http.route('/stock/categorias', type='http', auth='public', website=True)
     def render_example_page(self):
         categorias = http.request.env['Categorias'].sudo().search([])
         return http.request.render('myfirst_model.vistaCategoria',{'Categorias':categorias})
