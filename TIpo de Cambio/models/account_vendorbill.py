9daed2a6ab8829ec98abad9c7e292c2844644c1d
viernes <shelonga@live.com>
2019-04-10 15:18:06 -0500

    purchase

--- TIpo de Cambio/models/account_vendorbill.py
+++ TIpo de Cambio/models/account_vendorbill.py
@@ -3,15 +3,8 @@ from odoo import api, fields, models, _
 class InvoiceCambio(models.Model):
     _inherit = 'account.invoice'
     tasadecambiobill = fields.Float(related='currency_id.rate_ids.rate')
-    cambiobill = fields.Float(string='Tipo de Cambio',digits=(12,3),compute='CalcularCambio',store=True)
+    cambiobill = fields.Float(string='Tipo de Cambio',digits=(12,3),store=True)
    
-    @api.depends('tasadecambiobill')
-    def CalcularCambio(self):
-        for record in self:
-        	if (record.cambiobill==0 and record.tasadecambiobill!=0):
-        		record['cambiobill'] = 1/record.tasadecambiobill
-        	else:
-        		pass
 
    
 
