0b0c716538c926c18b50c48183979771b0a3d836
viernes <shelonga@live.com>
2019-04-10 14:48:04 -0500

    purchase

--- TIpo de Cambio/models/__init__.py
+++ TIpo de Cambio/models/__init__.py
@@ -1,5 +1,5 @@
 from . import account_invoice
 from . import sale
-
 from . import purchase
+from . import account_vendorbill
 from . import sale_make_invoice_advance_payment
--- TIpo de Cambio/models/purchase.py
+++ TIpo de Cambio/models/purchase.py
@@ -1,21 +1,21 @@
 from odoo import api, fields, models, _
 
-class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
     tasadecambio = fields.Float(related='currency_id.rate_ids.rate')
-    cambio = fields.Float(string='Tipo de Cambio',digits=(12,3),compute='CalcularCambio',store=True)
+    cambiobill = fields.Float(string='Tipo de Cambio',digits=(12,3),compute='CalcularCambio',store=True)
    
     @api.depends('tasadecambio')
     def CalcularCambio(self):
         for record in self:
         	if (record.tasadecambio!=0):
         		record['cambio'] = 1/record.tasadecambio
+
     @api.multi
-    def create_bill(self):
-        res = super(SaleOrder, self).create_bill()
+    def _prepare_invoice(self):
+        res = super(PurchaseOrder, self)._prepare_invoice()
         res.update({
-            'cambio':self.cambio,
+            'cambiobill':self.cambiobill,
             })
         return res
-
 
