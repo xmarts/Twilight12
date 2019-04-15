435c5165bf061b29f9ec980e0c5b613dc76be097
viernes <shelonga@live.com>
2019-04-15 11:44:34 -0500

    purchase field view

--- TIpo de Cambio/models/sale.py
+++ TIpo de Cambio/models/sale.py
@@ -4,7 +4,7 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
     tasadecambio = fields.Float(related='currency_id.rate_ids.rate')
     cambio = fields.Float(string='Tipo de Cambio',digits=(12,3),compute='CalcularCambio',store=True,readonly=0,default='1')
-    change=fields.Float(compute='cambioextra')
+    change=fields.Float(compute='cambioextra',readonly=0,default='1')
     @api.onchange('cambio')
     def cambioextra(self):
         for record in self:
