257c5c94ded326c4fc32acc1394e0e35a937c900
viernes <shelonga@live.com>
2019-04-15 13:39:58 -0500

    purchase field view

--- TIpo de Cambio/models/currency.py
+++ TIpo de Cambio/models/currency.py
@@ -4,7 +4,7 @@ class SaleOrderC(models.Model):
     _inherit = 'res.currency'
     cambio = fields.Float(string='Tasa Comercial',digits=(12,3),compute='CalcularCambio',store=True,readonly=0)
    
-    @api.depends('rate)
+    @api.depends('rate')
     def CalcularCambio(self):
         for record in self:
         	if (record.rate!=0):
