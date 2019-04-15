2637f14e34e45e3e0f78d88a013edaeea8502bb7
viernes <shelonga@live.com>
2019-04-15 12:53:29 -0500

    purchase field view

---
+++ TIpo de Cambio/models/currency.py
@@ -0,0 +1,14 @@
+from odoo import api, fields, models, _
+
+class SaleOrder(models.Model):
+    _inherit = 'res.currency'
+    cambio = fields.Float(string='Tasa Comercial',digits=(12,3),compute='CalcularCambio',store=True,readonly=0)
+   
+    @api.depends('rate)
+    def CalcularCambio(self):
+        for record in self:
+        	if (record.rate!=0):
+        		record['cambio'] = 1/record.rate
+ 
+
+
