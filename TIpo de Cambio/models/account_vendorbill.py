2851bc6c48ce1a0e1c09320063a6c31bed397654
viernes <shelonga@live.com>
2019-04-22 14:53:01 -0500

    Debug and Optimizev2 4

--- TIpo de Cambio/models/account_vendorbill.py
+++ TIpo de Cambio/models/account_vendorbill.py
@@ -3,17 +3,18 @@ from odoo import api, fields, models, _
 class InvoiceCambio(models.Model):
     _inherit = 'account.invoice'
     tasadecambiobill = fields.Float(related='currency_id.rate_ids.rate')
-    cambiobill = fields.Float(string='Tipo de Cambio',digits=(12,3),store=True,compute='calcularcambio2')
+    cambiobill = fields.Float(string='Tipo de Cambio',digits=(12,3),store=True,compute='calcularcambio2',readonly=0)
 
 
     @api.onchange('purchase_id')
     def calcularcambio2(self):
-        porder=record.origin #purchase order name 
-        if porder:
-            res=self.env['purchase.order'].sudo().search([('name','=',porder)]).cambiobill #busqueda en purchase order en donde el nombre sea igual 'porder' y te regesa el '.'cambiobill de la purchase order
-            record[("cambiobill")]=res  #iguala el tipo de cambio al resultado de la busqueda
-        elif (record.tasadecambio!=0):
-            record['cambiobill'] = 1/record.tasadecambio
+        for record in self:
+            porder=record.origin #purchase order name 
+            if porder:
+                res=self.env['purchase.order'].sudo().search([('name','=',porder)]).cambiobill #busqueda en purchase order en donde el nombre sea igual 'porder' y te regesa el '.'cambiobill de la purchase order
+                record[("cambiobill")]=res  #iguala el tipo de cambio al resultado de la busqueda
+            elif (record.tasadecambio!=0):
+                record['cambiobill'] = 1/record.tasadecambio
     
    
 
--- TIpo de Cambio/models/sale.py
+++ TIpo de Cambio/models/sale.py
@@ -3,7 +3,7 @@ from odoo import api, fields, models, _
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
     tasadecambio = fields.Float(related='currency_id.rate_ids.rate')
-    cambio = fields.Float(string='Tipo de Cambio Actual',digits=(12,3),compute='CalcularCambio',store=True,readonly=0,default='1')
+    cambio = fields.Float(string='Tipo de Cambio',digits=(12,3),compute='CalcularCambio',store=True,readonly=0,default='1')
 
     @api.onchange('currency_id')
     def CalcularCambio(self):
