e98d6e72f8e2c808f303f61bc38ad62ecaac072f
viernes <shelonga@live.com>
2019-04-21 21:30:42 -0500

    Debug and Optimize 4

--- TIpo de Cambio/models/account_invoice.py
+++ TIpo de Cambio/models/account_invoice.py
@@ -8,10 +8,10 @@ class InvoiceCambio(models.Model):
     @api.onchange('currency_id')
     def calcularcambio(self):
         for record in self:
-        	if (record.tasadecambio !=0):
-        		record['cambio'] = 1/record.tasadecambio
-        	else:
-        		pass
+            if (record.tasadecambio !=0):
+                record['cambio'] = 1/record.tasadecambio
+            else:
+                pass
 
    
 
--- TIpo de Cambio/models/sale.py
+++ TIpo de Cambio/models/sale.py
@@ -8,10 +8,10 @@ class SaleOrder(models.Model):
     @api.onchange('currency_id')
     def CalcularCambio(self):
         for record in self:
-        	if (record.tasadecambio!=0):
-        		record['cambio'] = 1/record.tasadecambio
-		else:
-			pass
+            if (record.tasadecambio!=0):
+                record['cambio'] = 1/record.tasadecambio
+            else:
+                pass
 
     @api.multi
     def _prepare_invoice(self):
