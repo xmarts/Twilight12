fb75ec3778cef4a07180682fb7dcd182bca5c1aa
viernes <shelonga@live.com>
2019-04-22 16:08:08 -0500

    Debug and Optimizev3 2

--- TIpo de Cambio/models/account_vendorbill.py
+++ TIpo de Cambio/models/account_vendorbill.py
@@ -3,11 +3,11 @@ from odoo import api, fields, models, _
 class InvoiceCambio(models.Model):
     _inherit = 'account.invoice'
     cambiobill = fields.Float(string='Tipo de Cambio',digits=(12,3),store=True,compute='calcularcambio2',readonly=0)
-    cambiobill2 = fields.Char(string='Tipo de Cambio',digits=(12,3),store=True,compute='calcularcambio2',readonly=0)
+    cambiobill2 = fields.Char(string='Tipo de Cambio',store=True,compute='calcularcambio3',readonly=0)
 
 
     @api.onchange('currency_id','partner_id')
-    def calcularcambio2(self):
+    def calcularcambio3(self):
         x=self.origin
         resultado=self.env['purchase.order'].sudo().search([('name','=',x)])#busqueda en purchase order en donde el nombre sea igual 'porder' y te regesa el '.'cambiobill de la purchase order
         for record in self:
