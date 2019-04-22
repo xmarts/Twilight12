de5d738e70b1a5facea8a13fe1c680a7eb06e096
viernes <shelonga@live.com>
2019-04-22 15:42:19 -0500

    Debug and Optimizev3 1

--- TIpo de Cambio/models/account_vendorbill.py
+++ TIpo de Cambio/models/account_vendorbill.py
@@ -2,15 +2,13 @@ from odoo import api, fields, models, _
 
 class InvoiceCambio(models.Model):
     _inherit = 'account.invoice'
-    tasadecambiobill = fields.Float(related='currency_id.rate_ids.rate')
     cambiobill = fields.Float(string='Tipo de Cambio',digits=(12,3),store=True,compute='calcularcambio2',readonly=0)
 
     @api.multi
-    @api.onchange('currency_id')
+    @api.onchange('currency_id','partner_id')
     def calcularcambio2(self):
         for record in self:
             porder=record.origin #purchase order name
-            raise exceptions.Warning(porder)  
             if porder:
                 resultado=self.env['purchase.order'].sudo().search([('name','=',porder)]).cambiobill #busqueda en purchase order en donde el nombre sea igual 'porder' y te regesa el '.'cambiobill de la purchase order
                 record[("cambiobill")]=float(resultado)  #iguala el tipo de cambio al resultado de la busqueda
