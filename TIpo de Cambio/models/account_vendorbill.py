8b0bfd488c23d0a347568b0e44f927e27caeca50
viernes <shelonga@live.com>
2019-04-22 15:12:51 -0500

    Debug and Optimizev2 5

--- TIpo de Cambio/models/account_vendorbill.py
+++ TIpo de Cambio/models/account_vendorbill.py
@@ -5,12 +5,12 @@ class InvoiceCambio(models.Model):
     tasadecambiobill = fields.Float(related='currency_id.rate_ids.rate')
     cambiobill = fields.Float(string='Tipo de Cambio',digits=(12,3),store=True,compute='calcularcambio2',readonly=0)
 
-
+    @api.multi
     @api.onchange('currency_id')
     def calcularcambio2(self):
         for record in self:
             porder=record.origin #purchase order name 
-            if porder:
+            if porder == True:
                 res=self.env['purchase.order'].sudo().search([('name','=',porder)]).cambiobill #busqueda en purchase order en donde el nombre sea igual 'porder' y te regesa el '.'cambiobill de la purchase order
                 record[("cambiobill")]=res  #iguala el tipo de cambio al resultado de la busqueda
             elif (record.tasadecambio!=0):
