cee2edbeb806416798fd80bd8f21bba3bef3b04e
viernes <shelonga@live.com>
2019-04-22 16:06:44 -0500

    Debug and Optimizev3 1

--- TIpo de Cambio/models/account_vendorbill.py
+++ TIpo de Cambio/models/account_vendorbill.py
@@ -3,14 +3,23 @@ from odoo import api, fields, models, _
 class InvoiceCambio(models.Model):
     _inherit = 'account.invoice'
     cambiobill = fields.Float(string='Tipo de Cambio',digits=(12,3),store=True,compute='calcularcambio2',readonly=0)
+    cambiobill2 = fields.Char(string='Tipo de Cambio',digits=(12,3),store=True,compute='calcularcambio2',readonly=0)
 
-    @api.multi
+
     @api.onchange('currency_id','partner_id')
     def calcularcambio2(self):
+        x=self.origin
+        resultado=self.env['purchase.order'].sudo().search([('name','=',x)])#busqueda en purchase order en donde el nombre sea igual 'porder' y te regesa el '.'cambiobill de la purchase order
         for record in self:
-            porder=record.origin #purchase order name
+            record[("cambiobill2")]=resultado  #iguala el tipo de cambio al resultado de la busqueda
+       
+    @api.onchange('currency_id','partner_id')
+    def calcularcambio2(self):
+        x=self.origin
+        resultado=self.env['purchase.order'].sudo().search([('name','=',x)]).cambiobill
+         #busqueda en purchase order en donde el nombre sea igual 'porder' y te regesa el '.'cambiobill de la purchase order
+        for record in self:
             if porder:
-                resultado=self.env['purchase.order'].sudo().search([('name','=',porder)]).cambiobill #busqueda en purchase order en donde el nombre sea igual 'porder' y te regesa el '.'cambiobill de la purchase order
                 record[("cambiobill")]=float(resultado)  #iguala el tipo de cambio al resultado de la busqueda
             elif (record.tasadecambio!=0):
                 record[('cambiobill')] = 1/record.tasadecambio
