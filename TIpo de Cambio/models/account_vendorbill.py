ce533ec043aa37c6dc636940664460f3b0374d19
viernes <shelonga@live.com>
2019-04-23 09:47:48 -0500

    Debug and Optimizev4

--- TIpo de Cambio/models/account_vendorbill.py
+++ TIpo de Cambio/models/account_vendorbill.py
@@ -3,30 +3,22 @@ from odoo import api, fields, models, _
 class InvoiceCambio(models.Model):
     _inherit = 'account.invoice'
     cambiobill = fields.Float(string='Tipo de Cambio',digits=(12,3),store=True,compute='calcularcambio3',readonly=0)
-    cambiobill2 = fields.Char(string='Tipo de Cambio',store=True,compute='calcularcambio3',readonly=0)
+
 
 
     @api.onchange('currency_id','partner_id')
     def calcularcambio3(self):
         for record in self:
             x=record.origin
-            domain=[('name','=',x)] 
-            dicts=self.env['purchase.order'].sudo().search_read(domain,[('cambiobill')])
+            if x:
+                domain=[('name','=',x)] 
+                dicts=self.env['purchase.order'].sudo().search_read(domain,[('cambiobill')])
             #busqueda en purchase order en donde el nombre sea igual 'porder' y te regesa el '.'cambiobill de la purchase order
-            for item in dicts:
-                resultado=item['cambiobill']
-            record[("cambiobill")]=float(resultado)  #iguala el tipo de cambio al resultado de la busqueda
-       
-    #@api.onchange('currency_id','partner_id')
-    #def calcularcambio2(self):
-     #   x=self.origin
-      #  resultado=self.env['purchase.order'].sudo().search([('name','=',x)]).cambioq
-         #busqueda en purchase order en donde el nombre sea igual 'porder' y te regesa el '.'cambiobill de la purchase order
-       # for record in self:
-        #    if x:
-         #       record[("cambiobill")]=float(resultado)  #iguala el tipo de cambio al resultado de la busqueda
-          #  elif (record.tasadecambio!=0):
-           #     record[('cambiobill')] = 1/record.tasadecambio
+                for item in dicts:
+                    resultado=item['cambiobill']
+                record[("cambiobill")]=float(resultado)  #iguala el tipo de cambio al resultado de la busqueda
+            elif (record.tasadecambio!=0):
+                record[('cambiobill')] = 1/record.tasadecambio
     
    
 
