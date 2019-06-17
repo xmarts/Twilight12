69f1297c4b3cd2071992bc6b8a80b226626d0903
viernes <shelonga@live.com>
2019-06-17 15:03:42 -0500

    Qty_auto 2

--- xmarts_auto_qty/models/sale_order.py
+++ xmarts_auto_qty/models/sale_order.py
@@ -2,7 +2,7 @@ from odoo import api, fields, models, _
 
 class SaleSignature(models.Model):
 	_inherit = 'sale.order.line'
-	product_uom_qty = fields.Float(default=2.0)
+	product_uom_qty = fields.Float(default=1.0)
 	
 
 
