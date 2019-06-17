43e583b36e1c5e079d8398fe1085b509e516bfb6
viernes <shelonga@live.com>
2019-06-17 11:32:20 -0500

    Qty_auto

---
+++ xmarts_auto_qty/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Xmarts auto qty  1',
+    'version': '11.0.3',
+    'category': "",
+    'description': """ Set qty = 1 when selecting a product in an order line.
+    """,
+    'author':'Axel',
+    'depends': ['base','sale'],
+    'data': [
+	
+
+    ],
+    'qweb': [
+        ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+
+}
