cdeb4bc29839b6f14297d0930e192a5e1424c930
viernes <shelonga@live.com>
2019-06-17 13:10:36 -0500

    Qty_auto 2

--- xmarts_auto_qty/models/sale_order.py
+++ xmarts_auto_qty/models/sale_order.py
@@ -2,11 +2,8 @@ from odoo import api, fields, models, _
 
 class SaleSignature(models.Model):
 	_inherit = 'sale.order.line'
-	product_uom_qty = fields.Float(compute="_compute_change",store=True)
-	@api.onchange("product_id")
-	def _compute_change(self):
-		for record in self:
-			record[("product_uom_qty")]= 2
+	product_uom_qty = fields.Float(default=2.0)
+	
 
 
 
