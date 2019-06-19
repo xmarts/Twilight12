432b8e2f8be8a62cf1375b0c60d5babb1295b190
viernes <shelonga@live.com>
2019-06-19 18:01:36 -0500

    Module auto_configure add Country *Complete

--- xmarts_auto_qty/models/sale_order.py
+++ xmarts_auto_qty/models/sale_order.py
@@ -3,8 +3,9 @@ from odoo import api, fields, models, _
 class SaleSignature(models.Model):
 	_inherit = 'sale.order.line'
 	def get_sale_order_line_multiline_description_sale(self, product):
+		result=self.default_code +" "+ self.product_id.name
 
-		return "PRUEBA"
+		return result
 	
 
 
