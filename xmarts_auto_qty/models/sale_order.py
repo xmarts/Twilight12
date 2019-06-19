bf7e0ecb6d0ccaf7e894c7c1e889033f01a122d6
viernes <shelonga@live.com>
2019-06-19 18:04:47 -0500

    Module custom Description *Complete

--- xmarts_auto_qty/models/sale_order.py
+++ xmarts_auto_qty/models/sale_order.py
@@ -3,7 +3,7 @@ from odoo import api, fields, models, _
 class SaleSignature(models.Model):
 	_inherit = 'sale.order.line'
 	def get_sale_order_line_multiline_description_sale(self, product):
-		result=self.default_code +" "+ self.product_id.name
+		result=self.product_id.default_code +" "+ self.product_id.name
 
 		return result
 	
