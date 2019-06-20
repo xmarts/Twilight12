cbae245ec2fe670fd454c928045215fc7ae4c90e
viernes <shelonga@live.com>
2019-06-20 15:48:25 -0500

    Module boton test

---
+++ xmarts_flag/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Xmarts flag',
+    'version': '11.0.3',
+    'category': "",
+    'description': """ Set qty = 1 when selecting a product in an order line.
+    """,
+    'author':'Axel',
+    'depends': ['base','sale'],
+    'data': [
+	
+
+    ],
+    'qweb': [
+        ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+
+}
---
+++ xmarts_flag/models/sale_order.py
@@ -0,0 +1,22 @@
+from odoo import api, fields, models, _
+
+class saleorder(models.Model):
+	_inherit = 'sale.order'
+
+	boton1=fields.Boolean(string="Switch")
+	boton2=fields.Boolean(string="Switch")
+
+	@api.onchange('boton2','boton1')
+	def switch2(self):
+		for record in self:
+			record.boton1=!record.boton1
+			record.boton2=!record.boton2
+	
+
+
+
+
+
+
+
+
