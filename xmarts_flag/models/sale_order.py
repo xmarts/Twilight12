e8bfc68d6b98c599d07f4d64427fc35da17fbf0e
viernes <shelonga@live.com>
2019-06-20 16:12:08 -0500

    boton

--- xmarts_flag/models/sale_order.py
+++ xmarts_flag/models/sale_order.py
@@ -3,14 +3,14 @@ from odoo import api, fields, models, _
 class saleorder(models.Model):
 	_inherit = 'sale.order'
 
-	boton1=fields.Boolean()
-	boton2=fields.Boolean()
+	boton1=fields.Boolean(default=True)
+	boton2=fields.Boolean(default=False)
 
 	@api.onchange('boton2','boton1')
 	def switch2(self):
 		for record in self:
-			record.boton1=!record.boton1
-			record.boton2=!record.boton2
+			record[("boton1")]= not record.boton1
+			record[("boton2")]= not record.boton2
 	
 
 
