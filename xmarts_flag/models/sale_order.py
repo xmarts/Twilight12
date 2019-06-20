24857911ded29e01acf2654922ee3cb1d39cb7dc
viernes <shelonga@live.com>
2019-06-20 16:30:08 -0500

    boton prueba con self

--- xmarts_flag/models/sale_order.py
+++ xmarts_flag/models/sale_order.py
@@ -3,8 +3,8 @@ from odoo import api, fields, models, _
 class SaleOrder(models.Model):
 	_inherit = 'sale.order'
 
-	boton1=fields.Boolean(default=True,compute="switch2")
-	boton2=fields.Boolean(default=False,compute="switch2")
+	boton1=fields.Boolean(default=True,compute="switch2",readonly=0)
+	boton2=fields.Boolean(default=False,compute="switch2",readonly=0)
 
 	@api.onchange('boton2','boton1')
 	def switch2(self):
