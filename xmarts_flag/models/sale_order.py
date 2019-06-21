3a511ff43fdff7fe844bbcb8edb7963bd0108cf4
viernes <shelonga@live.com>
2019-06-21 10:24:48 -0500

    modificar nombre

--- xmarts_flag/models/sale_order.py
+++ xmarts_flag/models/sale_order.py
@@ -1,19 +1,11 @@
 from odoo import api, fields, models, _	
 
-class SaleOrder(models.Model):
-	_inherit = 'sale.order'
+class Pricelist(models.Model):
+	_inherit = 'product.pricelist'
 
-	boton1=fields.Boolean(default=True,compute="switch2",readonly=0)
-	boton2=fields.Boolean(default=False,compute="switch2",readonly=0)
-
-	@api.onchange('boton2','boton1')
-	def switch2(self):
-		for record in self:
-
-			record[("boton1")]= not record.boton1
-			record[("boton2")]= not record.boton2
-	
-
+	@api.multi
+	def name_get(self):
+		return [(pricelist.id, '%s' % (pricelist.name)) for pricelist in self]
 
 
 
