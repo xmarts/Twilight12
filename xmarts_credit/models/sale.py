d85aae37795d5d332e3e53d141ff4c71fb5af273
viernes <shelonga@live.com>
2019-06-24 13:55:19 -0500

    prueba cantidad transito fix

--- xmarts_credit/models/sale.py
+++ xmarts_credit/models/sale.py
@@ -1,29 +1,23 @@
 from odoo import api, fields, models, _
 
 class stockmovelinesp(models.Model):
-	_inherit = 'stock.move.line'
+    _inherit = 'stock.move.line'
+
+    @api.onchange('product_id')
+    def _compute_transito(self):
+        for record in self:
+            if record.done_move !=True and record.location_dest_id.id==13:
+                x=self.env['Product_template']
+                x._compute_transito_product(self)
 
 
-	@api.onchange('product_id')
-	def _compute_transito(self):
-		for record in self:
-			if record.done_move !=True and record.location_dest_id.id==13:
-				self.env['product.template']._compute_transito_product()
 
+class Product_template(models.Model):
+    _inherit = 'product.template'
 
-class producttemplate(models.Model):
-	_inherit = 'product.template'
-
-	cantidad_transito = fields.Char(string="cost" ,store=True,readonly=False)
-
-	@api.multi
-	def _compute_transito_product(self):
-		for record in self:
-			x = self.env['stock.move.line'].sudo().search([('product_id', '=', record.id)])
-
-			record[("cantidad_transito")] = 21
-
-
-	
-
-
+    cantidad_transito = fields.Char(string="cost" ,store=True,readonly=False)
+    @api.multi
+    def _compute_transito_product(self):
+        for record in self:
+            x = self.env['stock.move.line'].sudo().search([('product_id', '=', record.id)])
+            record[("cantidad_transito")] = 21
